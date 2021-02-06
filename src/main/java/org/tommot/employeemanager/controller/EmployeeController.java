3759f7c8403e5b6dfa6eff538112863245367880
Tosin <{ID}+{username}@users.noreply.github.com>
2021-02-06 04:38:21 +0100

    completed Employee Controller operations

--- src/main/java/org/tommot/employeemanager/controller/EmployeeController.java
+++ src/main/java/org/tommot/employeemanager/controller/EmployeeController.java
@@ -2,10 +2,7 @@ package org.tommot.employeemanager.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.tommot.employeemanager.model.Employee;
 import org.tommot.employeemanager.service.EmployeeService;
 
@@ -31,8 +28,28 @@ public class EmployeeController {
 
     @GetMapping("/find/{id}")
     public  ResponseEntity<Employee> getEmployeeById(@PathVariable("id") Long id){
+        //id comes from the Pathvariable
         Employee employee = employeeService.findEmployeeById(id);
         return  new ResponseEntity<>(employee, HttpStatus.OK);
+    }
 
+    @PostMapping("/add")
+    public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee){
+        // Request is coming from the RequestBody
+        Employee newEmployee = employeeService.addEmployee(employee);
+        return  new ResponseEntity<>(newEmployee, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee){
+        // Request is coming from the RequestBody
+        Employee updatedEmployee = employeeService.updateEmployees(employee);
+        return  new ResponseEntity<>(updatedEmployee, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> delteEmployee(@PathVariable("id") Long id){
+        employeeService.deleteEmployee(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
