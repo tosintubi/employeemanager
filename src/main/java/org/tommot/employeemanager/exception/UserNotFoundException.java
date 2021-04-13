f9ddaef2dce4f71febb094c1426b83421e54c5e8
Tosin <1065402+tosintubi@users.noreply.github.com>
2021-04-13 23:19:24 +0200

    Update UserNotFoundException.java

--- src/main/java/org/tommot/employeemanager/exception/UserNotFoundException.java
+++ src/main/java/org/tommot/employeemanager/exception/UserNotFoundException.java
@@ -1,9 +1,9 @@
 package org.tommot.employeemanager.exception;
 
 public class UserNotFoundException extends RuntimeException {
+    
     //Calls the constructor on the class and pass the "message
     public UserNotFoundException(String message) {
         super(message);
-
     }
 }
